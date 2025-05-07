0cec4d571c3095299b5a2809f394a4be780e17d8
danielpirela <danielapirelac@gmail.com>
2025-05-07 18:16:33 +0000

    d

--- prueba/src/App.jsx
+++ prueba/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Elite II danie</h1>
+      <h1>Elite II danielllll</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           Enercusi zazazsssza {count}
