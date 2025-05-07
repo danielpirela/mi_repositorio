496c6f1c4ae7bcf93b7f6ec8580f87192ff33254
danielpirela <danielapirelac@gmail.com>
2025-05-07 18:13:14 +0000

    x

--- prueba/src/App.jsx
+++ prueba/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Elite II danie</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          Enercusi zazazsssa {count}
+          Enercusi zazazsssza {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
