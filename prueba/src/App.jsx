b976f175d96d2c91d2dcb206a1679bd93a6d2f94
daniel pirela <danielapirelac@gmail.com>
2025-05-07 16:53:51 -0400

    asmdbamsd

--- prueba/src/App.jsx
+++ prueba/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit ciclasds<code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">learn moreeee chao hola</p>
+      <p className="read-the-docs">learn smoreeee chao hola</p>
     </>
   )
 }
